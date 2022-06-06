cd5ebe811bad8dfe032456deeae266798e6c5162
Michael <michaelfredywijaya@gmail.com>
2022-06-06 10:20:19 +0700

    organizing files and folders

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const usersRouter = require('../api/users/user-router');
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.json({
-    status: 'success',
-    message: 'This API is working!',
-  });
-});
+app.use('/api/users', usersRouter);
 
 app.listen(process.env.APP_PORT, process.env.DB_HOST, () => {
   console.log('Server is running on PORT : ', process.env.APP_PORT);
